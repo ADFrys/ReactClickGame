667e7aeb04de006b26bd915883bf56bd3d50703c
AngelaF <ADFrys@users.noreply.github.com>
2018-04-18 13:26:22 -0500

    Make code DRY on main page.

--- reactclickgame/src/App.js
+++ reactclickgame/src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import FriendCard from "./components/FriendCard";
 import characters from "./characters.json";
 
-const App = () => (
-  <Wrapper>
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    characters
+  };
+
+
+
+  clickedCard = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const characters = this.state.characters.filter(character => character.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ characters });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
   <div className="container">
     <div className="panel panel-default">
       <div className="panel-heading">
@@ -21,80 +39,19 @@ const App = () => (
       </div>
     </div>
   </div>
-      <Title>Clicky Game</Title>
-    <FriendCard
-      name={characters[0].name}
-      image={characters[0].image}
-      occupation={characters[0].occupation}
-      location={characters[0].location}
-    />
-    <FriendCard
-      name={characters[1].name}
-      image={characters[1].image}
-      occupation={characters[1].occupation}
-      location={characters[1].location}
-    />
-    <FriendCard
-      name={characters[2].name}
-      image={characters[2].image}
-      occupation={characters[2].occupation}
-      location={characters[2].location}
-    />
-    <FriendCard
-      name={characters[3].name}
-      image={characters[3].image}
-      occupation={characters[3].occupation}
-      location={characters[3].location}
-    />
-    <FriendCard
-      name={characters[4].name}
-      image={characters[4].image}
-      occupation={characters[4].occupation}
-      location={characters[4].location}
-    />
-    <FriendCard
-      name={characters[5].name}
-      image={characters[5].image}
-      occupation={characters[5].occupation}
-      location={characters[5].location}
-    />
-    <FriendCard
-      name={characters[6].name}
-      image={characters[6].image}
-      occupation={characters[6].occupation}
-      location={characters[6].location}
-    />
-     <FriendCard
-      name={characters[7].name}
-      image={characters[7].image}
-      occupation={characters[7].occupation}
-      location={characters[7].location}
-    />
-    <FriendCard
-      name={characters[8].name}
-      image={characters[8].image}
-      occupation={characters[8].occupation}
-      location={characters[8].location}
-    />
-    <FriendCard
-      name={characters[9].name}
-      image={characters[9].image}
-      occupation={characters[9].occupation}
-      location={characters[9].location}
-    />
-    <FriendCard
-      name={characters[10].name}
-      image={characters[10].image}
-      occupation={characters[10].occupation}
-      location={characters[10].location}
-    />
-    <FriendCard
-      name={characters[11].name}
-      image={characters[11].image}
-      occupation={characters[11].occupation}
-      location={characters[11].location}
-    />
-  </Wrapper>
-);
+        <Title>Game Time!</Title>
+        {this.state.characters.map(character => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={character.id}
+            key={character.id}
+            name={character.name}
+            image={character.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
 
 export default App;
